5d1129ce36fa4b48d9c0af0714c3861e89ea0938
Vasichar11 <vasichar11@hotmail.com>
2021-09-25 18:08:16 +0300

    just constants

--- Bell_Telescope/headers/constants.h
+++ Bell_Telescope/headers/constants.h
@@ -53,9 +53,9 @@ const real telescope_lamda = 30; //90 //0,10,20,30
 
 //--Distribution parameters--//
 const int64_t eta_dstr    = 1;					//Number of different values for each distribution.										 
-const int64_t aeq_dstr    = 10;  			    
+const int64_t aeq_dstr    = 100;  			    
 const int64_t lamda_dstr  = 10;					
-const int64_t population  = eta_dstr * aeq_dstr * lamda_dstr;
+const int64_t test_pop    = eta_dstr * aeq_dstr * lamda_dstr;   //Testing population. Not all of them will be tracked. Only those will valid initial state.
 
 //------------------------------ Eta distribution range and step. --------------------------------//
 const real eta_start_d    = eta0_deg;     //Degrees 	 
@@ -68,8 +68,8 @@ const real aeq_end_d      = 179;
 const real aeq_step_d	  = (aeq_end_d - aeq_start_d)/(aeq_dstr-1); 	
 		                          
 //----------------------------- Lamda distribution range and step. -------------------------------//					 
-const real lamda_start_d  =0;     					 				
+const real lamda_start_d  = -90;     					 				
 const real lamda_end_d    = 90;   			
-const real lamda_step_d	  = 0;//(lamda_end_d - lamda_start_d)/(lamda_dstr-1); 	
+const real lamda_step_d	  = (lamda_end_d - lamda_start_d)/(lamda_dstr-1); 	
 };	
 
