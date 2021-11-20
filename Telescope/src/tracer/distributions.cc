877a92a65023bcbd1b52d3d510d1a0f93f81aa2a
Vasichar11 <vasichar11@hotmail.com>
2021-11-20 14:41:25 +0200

    correct dstr

--- Telescope/src/tracer/distributions.cc
+++ Telescope/src/tracer/distributions.cc
@@ -9,7 +9,7 @@
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(halfmean, std); 
     double number;
-    std::array<real,Constants::aeq_dstr> da;
+    std::array<real,Constants::aeq_dstr/2 + 1> da;
 
 	//"Half-normal" dstr for da
     for(int i=0;i<=Constants::aeq_dstr/2;i++)
@@ -18,16 +18,25 @@
         if(number>=0) da.at(i) = number; 
         else i--; //sample again
     }
-    std::cout<<"\n"<<da.begin();
-    std::sort(da.begin(), da.begin()+Constants::aeq_dstr/2,std::greater<int>()); //Sort the da[] high->low
+    std::sort(da.begin(), da.end()); //Sort the da[] low->high
   
-    for(int i=1; i<Constants::aeq_dstr/2; i++)  da.at(Constants::aeq_dstr/2 + i) =  da.at(Constants::aeq_dstr/2 - i);
 
-    for(int i=0;i<Constants::aeq_dstr;i++)  std::cout<<"\n"<<da.at(i);
+    for(int i=0;i<Constants::aeq_dstr/2;i++)  std::cout<<"\n"<<da.at(i);
 
-
+    std::cout<<"\n\n\n\n";
+    
 	//Shift degrees. Symmetrical dstr arround mean=(shifting value)
-	for(int i=0; i<Constants::aeq_dstr; i++)  dstr.at(i) += shift;
+	for(int i=1; i<=Constants::aeq_dstr/2; i++)
+    {
+        dstr.at(Constants::aeq_dstr/2 + i - 1) = shift + da.at(i);
+        dstr.at(Constants::aeq_dstr/2 - i)     = shift - da.at(i);
+    }
+
+    
+    
+    
+    
+    for(int i=0; i<Constants::aeq_dstr; i++)  std::cout<<"\n"<<dstr.at(i);
   
     return dstr;
 }
