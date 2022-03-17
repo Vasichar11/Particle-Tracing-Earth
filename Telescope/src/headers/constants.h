10814073ec689542c13fd84e7cd5b6f8f95d647c
Vasichar11 <vasichar11@hotmail.com>
2022-03-18 01:24:14 +0200

    upload pngs

--- Telescope/src/headers/constants.h
+++ Telescope/src/headers/constants.h
@@ -75,7 +75,7 @@ const real lamda_end_d    = 90;
 const real lamda_step_d	  = (lamda_end_d - lamda_start_d)/(lamda_dstr-1); 
 const real mean_aeq       = 90;  //Mean of the normal dstr
 const real mean_lamda     = 0; 
-const real stdev_aeq      = 18;  //Standard deviation of the normal dstr
+const real stdev_aeq      = 16;  //Standard deviation of the normal dstr
 const real stdev_lamda    = 30;  
 
 //Eta dstr
