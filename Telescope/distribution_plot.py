e45c0c7bc322a32beb9779408125842c59cd142c
Vasichar11 <vasichar11@ee.duth.gr>
2022-04-05 19:11:29 +0300

    negl

--- Telescope/distribution_plot.py
+++ Telescope/distribution_plot.py
@@ -11,33 +11,33 @@ sector_range = 10 #P.A bins #1deg
 sectors = int(view/sector_range)
 
 #Distribution read
-f1 = h5py.File("h5files/2000p_normalAEQ_normalLAMDA.h5","r")
+f1 = h5py.File("h5files/200000p_normalAEQ_normalLAMDA.h5","r")
 aeq0         = f1["aeq"][()]
 lamda0       = f1["lat"][()]
 aeq0_bins    = f1["aeq0_bins"][()]
 f1.close()
 
-f2 = h5py.File("h5files/2000p_both.h5","r")
-lamda00        = f2["lamda00"][()] #states when noWPI stops
-ppar00         = f2["ppar00"][()]
-pper00         = f2["pper00"][()]
-alpha00        = f2["alpha00"][()]
-aeq00          = f2["aeq00"][()]
-eta00          = f2["eta00"][()]
-time00         = f2["time00"][()]
+#f2 = h5py.File("h5files/2000p_both.h5","r")
+#lamda00        = f2["lamda00"][()] #states when noWPI stops
+#ppar00         = f2["ppar00"][()]
+#pper00         = f2["pper00"][()]
+#alpha00        = f2["alpha00"][()]
+#aeq00          = f2["aeq00"][()]
+#eta00          = f2["eta00"][()]
+#time00         = f2["time00"][()]
 
 
 #Save initials to CSV file
-header = ['id', 'aeq0_deg', 'lamda0_deg','aeq00_deg','lamda00_deg']
-data = []
-p=0
-for a0,l0,a00,l00 in zip(aeq0, lamda0, aeq00, lamda00):
-    data.extend([[p,a0*R2D,l0*R2D,a00*R2D,l00*R2D]])
-    p=p+1 #id is the element's location in the list since the particles were saved like that
-with open("dstr_before_and_after_WPI.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(data)
+#header = ['id', 'aeq0_deg', 'lamda0_deg','aeq00_deg','lamda00_deg']
+#data = []
+#p=0
+#for a0,l0,a00,l00 in zip(aeq0, lamda0, aeq00, lamda00):
+#    data.extend([[p,a0*R2D,l0*R2D,a00*R2D,l00*R2D]])
+#    p=p+1 #id is the element's location in the list since the particles were saved like that
+#with open("dstr_before_and_after_WPI.csv", "w") as f:
+#    writer = csv.writer(f)
+#    writer.writerow(header)
+#    writer.writerows(data)
 
 
 ######################################## PLOT INITIAL DISTRIBUTION #######################################
--- Telescope/src/headers/constants.h
+++ Telescope/src/headers/constants.h
@@ -50,8 +50,8 @@ const real pulse_duration=0.1;          		//Wave pulse duration in seconds.
 const real By_wave=1*pow(10,-9);  
 
 //--Simulation parameters--//
-const real t = 2;        				  	    //simulation time.
-const real t_nowpi = 2;          			    //no_WPI time.
+const real t = 14;        				  	    //simulation time.
+const real t_nowpi = 10;          			    //no_WPI time.
 const real t_wpi= t - t_nowpi;                  //WPI time.
 const real h=0.00001;						    //Runge kutta stepsize. Has to be much less than the particle's gyroperiod?
 const int64_t Nsteps_wpi  = t_wpi/h; 			//WPI step count
@@ -63,7 +63,7 @@ const real telescope_lamda = 0;
 
 //--Distribution parameters--//
 //const int64_t eta_dstr    = 1;					//Number of different values for each distribution.										 
-const int64_t aeq_dstr    = 1000;  	    		    
+const int64_t aeq_dstr    = 200000;  	    		    
 const int64_t lamda_dstr  = 1; //only one latitude per aeq					
 const int64_t population  = lamda_dstr * aeq_dstr;   
 const real aeq_start_d    = 1;    		  			 				
